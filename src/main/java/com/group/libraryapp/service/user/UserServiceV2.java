83bf0bc0376fa16ba9d2b12a00c16ddf83f4acc7
nhj <nhj1209526@naver.com>
2024-10-01 18:26:23 +0900

    Feat: 트랜잭션 추가

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,16 +20,22 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    // 아래 있는 함수가 시작될 때 start transaction;을 해준다 (트랜잭션을 시작)
+    // 함수가 예외 없이 잘 끝났다면 commit
+    // 혹시라도 문제가 있다면 rollback
+    @Transactional
     public void saveUser(UserCreateRequest request) {
-        User u = userRepository.save(new User(request.getName(), request.getAge()));
+        userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    @Transactional
     public List<UserResponse> getUsers() {
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         // select * from user where id = ?;
         User user = userRepository.findById(request.getId())
@@ -38,6 +45,7 @@ public class UserServiceV2 {
         userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name) {
         // SELECT * FROM user WHERE name = ?
         User user = userRepository.findByName(name);
