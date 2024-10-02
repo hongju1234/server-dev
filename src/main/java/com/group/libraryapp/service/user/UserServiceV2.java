d1c6e6602861ad27254bcb4683e686a542fec1d8
nhj <nhj1209526@naver.com>
2024-10-02 18:21:26 +0900

    Feat: 책 대출 기능 추가 (책 중복 대출x)

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -48,10 +48,8 @@ public class UserServiceV2 {
     @Transactional
     public void deleteUser(String name) {
         // SELECT * FROM user WHERE name = ?
-        User user = userRepository.findByName(name);
-        if (user == null) {
-            throw new IllegalArgumentException();
-        }
+        User user = userRepository.findByName(name)
+            .orElseThrow(IllegalArgumentException::new);
 
         userRepository.delete(user);
     }
